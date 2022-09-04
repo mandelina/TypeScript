2e509fe88567c520748e88788e61516ce383b710
minju moon <mmj9908@gmail.com>
2022-09-04 14:53:33 +0900

    :memo: localstorage API 구현해보기
    localstorage API 구현해보기

---
+++ class&interface/localstorage.ts
@@ -0,0 +1,26 @@
+// localstrage API
+interface SStorage<T> {
+  [key: string]: T;
+}
+
+class LocalStorage<T> {
+  private storage: SStorage<T> = {};
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): T {
+    return this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
+}
+
+const stringStorage = new LocalStorage<string>(); // string타입 로컬스토리지
+stringStorage.get("ket");
+
+const boolStorage = new LocalStorage<boolean>();
+boolStorage.get("xxx");

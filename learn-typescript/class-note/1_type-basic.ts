fe70697e5e9bd71cddc67f333fba8d77877c6a6a
MinjuMoon <mmj9908@greeneffect.co.kr>
2023-02-24 17:50:51 +0900

    TS 강의자료

---
+++ learn-typescript/class-note/1_type-basic.ts
@@ -0,0 +1,16 @@
+//JS 문자열 선언
+let str1 = 'hello';
+
+// TS문자열 선언
+let str2: string = 'hello';
+
+// TS 숫자
+let num: number = 10;
+
+//TS 배열
+let arr: Array<number> = [1, 2, 3];
+let items: number[] = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10]; //10은 에러
+
+//TS 튜플 : 배열의 위치에 type까지 지정
+let address: [string, number] = ['gangnam', 1];
